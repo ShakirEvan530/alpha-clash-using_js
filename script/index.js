375106136f50f6a28cb893db3bdf25db7ba71c3d
ShakirEvan530 <ausshakirahmed@gmail.com>
2024-02-16 11:58:29 +0600

    first main part done

--- script/index.js
+++ script/index.js
@@ -1,3 +1,32 @@
+// function play()
+//     {
+//         // hide the home screen . To hide the screen add the hidden class to the hom section 
+//         // Show the playground
+//         const homeSection = document.getElementById('home-screen');
+//         homeSection.classList.add('hidden')
+//         // console.log(homeSection.classList)
+
+//         // show the playground
+//         const playGroundSection = document.getElementById('play-ground');
+//         playGroundSection.classList.remove('hidden')
+//         // console.log(playGroundSection.classList)
+//     }
+
+function continueGame(){
+    // step-1 : generate a random alphabet
+    const alphabet = getARandomAlphabet();
+    console.log('Your Random Alphabet', alphabet);
+
+    // set a randomly genrated alphabet to the screen  (Show it)
+  const currentAlphabetElement = document.getElementById('current-alphabet');
+    currentAlphabetElement.innerText =alphabet;
+   
+   
+    // set background color 
+    setBackgroundColorById(alphabet);
+} 
 function play(){
-    console.log("Play Start Now");
+    hideElementById('home-screen');
+    showElementById('play-ground');
+    continueGame()
 }
